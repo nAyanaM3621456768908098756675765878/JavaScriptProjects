5bf8aa40fac82da32dea67d67ff562f9e4ac02de
Nayana M <nayana86600@gmail.com>
2025-12-27 16:26:00 +0530

    JScode2

--- 1Project Setup/quiz.js
+++
@@ -1,9 +0,0 @@
-const questionObj = 
-    {
-      category: 'Food & Drink',
-      id: 'qa-1',
-      correctAnswer: 'Three',
-      answers: ['Two', 'Three ', 'Four', 'Five'],
-      question:
-        "How many pieces of bun are in a Mcdonald's Big Mac?",
-    };
